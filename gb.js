6679fd709e1f1f3c26b361095471cc5771271d64
Triko <55321560+Triko21@users.noreply.github.com>
2020-10-22 22:45:33 +0200

    memory check

--- gb.js
+++ gb.js
@@ -27,23 +27,30 @@ var memory = new Uint8Array(0x10000); //64kb total memory
 var registar = new Uint8Array(8); //cpu reg
 
 //cpu reg addresses
-const A = 0; 
-const F = 1; 
-const B = 2; 
-const C = 3; 
-const D = 4; 
-const E = 5; 
-const H = 6; 
-const L = 7; 
+const A = (1 << 0); //Accumulator register
+const F = (1 << 1); //Accumulator flag
+const B = (1 << 2); 
+const C = (1 << 3); 
+const D = (1 << 4); 
+const E = (1 << 5); 
+const H = (1 << 6); // HL 2 Byte register used to store memory addresses
+const L = (1 << 7); // HL 2 Byte register used to store memory addresses
+
+var SP = 0; //stack pointer
+var PC = 0; //programm counter
 
 //Bus
-
 function write(addr, data ){
-    memory[addr] = data;
+    if(addr >= 0x0000 && addr <= 0xFFFF){
+        memory[addr] = data;
+    }     
 }
 
 function read(addr){
-    return memory(addr);
+    if(addr >= 0x0000 && addr <= 0xFFFF){
+        return memory(addr);
+    }
+    return 0x00
 }
 
 function dec2bin(dec){
