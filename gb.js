de2d03252a81b00c97feb19a1531d02d64944df0
Triko <55321560+Triko21@users.noreply.github.com>
2020-10-23 15:23:03 +0200

    added ld opcodes

--- gb.js
+++ gb.js
@@ -70,47 +70,89 @@ function ld(a,b){
     return 4;
 }
 
-function ld_imm(a){
-    register[a] = memory[PC+1];
+function ld_imm(a){ //loads next immediate byte from memory
+    register[a] = read(PC+1);
     PC += 2;
     return 8;
+}
+
+function ld_from_mem(a,b,c){ //loads from defined memory location
+    register[a] = read( (register[b]<<8) + register[c]);
+    PC += 1;
+    return 8;
+}
+
+function ld_from_mem_imm(a){ //loads from memory location, pointed to by next to bytes
+    register[a] = read( (read(PC+2)<<8) + read(PC+1));
+    PC += 3;
+    return 16;
+}
+
+function ld_to_mem(a,b,c){ //write to defined memory location
+    write((register[b]<<8) + register[a], register[c]);
+    PC += 1;
+    return 8;
+}
+
+function ld_to_mem_imm(a,b){
+    if(b == to_imm){ //write to next immediate mem location
+        write( (read(PC+2)<<8) + read(PC+1), register[a])
+        PC += 3;
+        return 16;
+    }else{ //ld from next immediate byte and write to other mem location
+        write((register[b]<<8) + register[a], read(PC+1));
+        PC += 3;
+        return 12; 
+    }  
+}
+
+function ldac(a,b){
+    if(a==A){ //read from (C)
+        register[a] == read(0xFF00 + register[b]);
+        PC += 1;
+        return 8;
+    } else{ //write A to (C)
+        write(0xFF00 + register[a], register[b]);
+        PC += 1;
+        return 8;
+    }
 }
 
 
 //opcode array
 opcodes = new Uint8Array(0x1000);
 
-opcodes[ 0x00 ] = nop(4);
+opcodes[ 0x00 ] = nop(4); //NOP
 opcodes[ 0x01 ] = 
-opcodes[ 0x02 ] = 
+opcodes[ 0x02 ] = ld_to_mem(B,C,A); //ld (BC),A
 opcodes[ 0x03 ] = 
 opcodes[ 0x04 ] = 
 opcodes[ 0x05 ] = 
-opcodes[ 0x06 ] = ld_imm(B);
+opcodes[ 0x06 ] = ld_imm(B); //ld B,n
 opcodes[ 0x07 ] = 
 opcodes[ 0x08 ] = 
 opcodes[ 0x09 ] = 
-opcodes[ 0x0A ] = 
+opcodes[ 0x0A ] = ld_from_mem(A,B,C); //ld A,(BC)
 opcodes[ 0x0B ] = 
 opcodes[ 0x0C ] = 
 opcodes[ 0x0D ] = 
-opcodes[ 0x0E ] = ld_imm(C);
+opcodes[ 0x0E ] = ld_imm(C); //ld C,n
 opcodes[ 0x0F ] = 
 opcodes[ 0x10 ] = 
 opcodes[ 0x11 ] = 
-opcodes[ 0x12 ] = 
+opcodes[ 0x12 ] = ld_to_mem(D,E,A); //ld (DE),A
 opcodes[ 0x13 ] = 
 opcodes[ 0x14 ] = 
 opcodes[ 0x15 ] = 
-opcodes[ 0x16 ] = ld_imm(D);
+opcodes[ 0x16 ] = ld_imm(D); //ld D,n
 opcodes[ 0x17 ] = 
 opcodes[ 0x18 ] = 
 opcodes[ 0x19 ] = 
-opcodes[ 0x1A ] = 
+opcodes[ 0x1A ] = ld_from_mem(A,D,E); //ld A,(DE)
 opcodes[ 0x1B ] = 
 opcodes[ 0x1C ] = 
 opcodes[ 0x1D ] = 
-opcodes[ 0x1E ] = ld_imm(E);
+opcodes[ 0x1E ] = ld_imm(E); //ld E,n
 opcodes[ 0x1F ] = 
 opcodes[ 0x20 ] = 
 opcodes[ 0x21 ] = 
@@ -118,7 +160,7 @@ opcodes[ 0x22 ] =
 opcodes[ 0x23 ] = 
 opcodes[ 0x24 ] = 
 opcodes[ 0x25 ] = 
-opcodes[ 0x26 ] = ld_imm(H);
+opcodes[ 0x26 ] = ld_imm(H); //ld H,n
 opcodes[ 0x27 ] = 
 opcodes[ 0x28 ] = 
 opcodes[ 0x29 ] = 
@@ -126,7 +168,7 @@ opcodes[ 0x2A ] =
 opcodes[ 0x2B ] = 
 opcodes[ 0x2C ] = 
 opcodes[ 0x2D ] = 
-opcodes[ 0x2E ] = ld_imm(L);
+opcodes[ 0x2E ] = ld_imm(L); //ld L,n
 opcodes[ 0x2F ] = 
 opcodes[ 0x30 ] = 
 opcodes[ 0x31 ] = 
@@ -134,7 +176,7 @@ opcodes[ 0x32 ] =
 opcodes[ 0x33 ] = 
 opcodes[ 0x34 ] = 
 opcodes[ 0x35 ] = 
-opcodes[ 0x36 ] = 
+opcodes[ 0x36 ] = ld_to_mem_imm(H,L); //ld (HL),n
 opcodes[ 0x37 ] = 
 opcodes[ 0x38 ] = 
 opcodes[ 0x39 ] = 
@@ -142,72 +184,72 @@ opcodes[ 0x3A ] =
 opcodes[ 0x3B ] = 
 opcodes[ 0x3C ] = 
 opcodes[ 0x3D ] = 
-opcodes[ 0x3E ] = 
+opcodes[ 0x3E ] = ld_imm(A); //ld A,#
 opcodes[ 0x3F ] = 
-opcodes[ 0x40 ] = 
-opcodes[ 0x41 ] = 
-opcodes[ 0x42 ] = 
-opcodes[ 0x43 ] = 
-opcodes[ 0x44 ] = 
-opcodes[ 0x45 ] = 
-opcodes[ 0x46 ] = 
-opcodes[ 0x47 ] = 
-opcodes[ 0x48 ] = 
-opcodes[ 0x49 ] = 
-opcodes[ 0x4A ] = 
-opcodes[ 0x4B ] = 
-opcodes[ 0x4C ] = 
-opcodes[ 0x4D ] = 
-opcodes[ 0x4E ] = 
-opcodes[ 0x4F ] = 
-opcodes[ 0x50 ] = 
-opcodes[ 0x51 ] = 
-opcodes[ 0x52 ] = 
-opcodes[ 0x53 ] = 
-opcodes[ 0x54 ] = 
-opcodes[ 0x55 ] = 
-opcodes[ 0x56 ] = 
-opcodes[ 0x57 ] = 
-opcodes[ 0x58 ] = 
-opcodes[ 0x59 ] = 
-opcodes[ 0x5A ] = 
-opcodes[ 0x5B ] = 
-opcodes[ 0x5C ] = 
-opcodes[ 0x5D ] = 
-opcodes[ 0x5E ] = 
-opcodes[ 0x5F ] = 
-opcodes[ 0x60 ] = 
-opcodes[ 0x61 ] = 
-opcodes[ 0x62 ] = 
-opcodes[ 0x63 ] = 
-opcodes[ 0x64 ] = 
-opcodes[ 0x65 ] = 
-opcodes[ 0x66 ] = 
-opcodes[ 0x67 ] = 
-opcodes[ 0x68 ] = 
-opcodes[ 0x69 ] = 
-opcodes[ 0x6A ] = 
-opcodes[ 0x6B ] = 
-opcodes[ 0x6C ] = 
-opcodes[ 0x6D ] = 
-opcodes[ 0x6E ] = 
-opcodes[ 0x6F ] = 
-opcodes[ 0x70 ] = 
-opcodes[ 0x71 ] = 
-opcodes[ 0x72 ] = 
-opcodes[ 0x73 ] = 
-opcodes[ 0x74 ] = 
-opcodes[ 0x75 ] = 
+opcodes[ 0x40 ] = ld(B,B); //ld B,B
+opcodes[ 0x41 ] = ld(B,C); //ld B,C
+opcodes[ 0x42 ] = ld(B,D); //ld B,D
+opcodes[ 0x43 ] = ld(B,E); //ld B,E
+opcodes[ 0x44 ] = ld(B,H); //ld B,H
+opcodes[ 0x45 ] = ld(B,L); //ld B,L
+opcodes[ 0x46 ] = ld_from_mem(B,H,L); //ld B,(HL)
+opcodes[ 0x47 ] = ld(B,A); //ld B,A
+opcodes[ 0x48 ] = ld(C,B); //ld C,B
+opcodes[ 0x49 ] = ld(C,C); //ld C,C
+opcodes[ 0x4A ] = ld(C,D); //ld C,D
+opcodes[ 0x4B ] = ld(C,E); //ld C,E
+opcodes[ 0x4C ] = ld(C,H); //ld C,H
+opcodes[ 0x4D ] = ld(C,L); //ld C,L
+opcodes[ 0x4E ] = ld_from_mem(C,H,L); //ld C,(HL)
+opcodes[ 0x4F ] = ld(C,A); //ld C,A
+opcodes[ 0x50 ] = ld(D,B); //ld D,B
+opcodes[ 0x51 ] = ld(D,C); //ld D,C
+opcodes[ 0x52 ] = ld(D,D); //ld D,D
+opcodes[ 0x53 ] = ld(D,E); //ld D,E
+opcodes[ 0x54 ] = ld(D,H); //ld D,H
+opcodes[ 0x55 ] = ld(D,L); //ld D,L
+opcodes[ 0x56 ] = ld_from_mem(D,H,L); //ld D,(HL)
+opcodes[ 0x57 ] = ld(D,A); //ld D,A
+opcodes[ 0x58 ] = ld(E,B); //ld E,B
+opcodes[ 0x59 ] = ld(E,C); //ld E,C
+opcodes[ 0x5A ] = ld(E,D); //ld E,D
+opcodes[ 0x5B ] = ld(E,E); //ld E,E
+opcodes[ 0x5C ] = ld(E,H); //ld E,H
+opcodes[ 0x5D ] = ld(E,L); //ld E,L
+opcodes[ 0x5E ] = ld_from_mem(E,H,L); //ld E,(HL)
+opcodes[ 0x5F ] = ld(E,A); //ld E,A
+opcodes[ 0x60 ] = ld(H,B); //ld H,B
+opcodes[ 0x61 ] = ld(H,C); //ld H,C
+opcodes[ 0x62 ] = ld(H,D); //ld H,D
+opcodes[ 0x63 ] = ld(H,E); //ld H,E
+opcodes[ 0x64 ] = ld(H,H); //ld H,H
+opcodes[ 0x65 ] = ld(H,L); //ld H,L
+opcodes[ 0x66 ] = ld_from_mem(H,H,L); //ld H,(HL)
+opcodes[ 0x67 ] = ld(H,A); //ld H,A
+opcodes[ 0x68 ] = ld(L,B); //ld L,B
+opcodes[ 0x69 ] = ld(L,C); //ld L,C
+opcodes[ 0x6A ] = ld(L,D); //ld L,D
+opcodes[ 0x6B ] = ld(L,E); //ld L,E
+opcodes[ 0x6C ] = ld(L,H); //ld L,H
+opcodes[ 0x6D ] = ld(L,L); //ld L,L
+opcodes[ 0x6E ] = ld_from_mem(L,H,L); //ld L,(HL)
+opcodes[ 0x6F ] = ld(L,A); //ld L,A
+opcodes[ 0x70 ] = ld_to_mem(H,L,B); //ld (HL),B
+opcodes[ 0x71 ] = ld_to_mem(H,L,C); //ld (HL),C
+opcodes[ 0x72 ] = ld_to_mem(H,L,D); //ld (HL),D
+opcodes[ 0x73 ] = ld_to_mem(H,L,E); //ld (HL),E
+opcodes[ 0x74 ] = ld_to_mem(H,L,H); //ld (HL),H
+opcodes[ 0x75 ] = ld_to_mem(H,L,L); //ld (HL),L
 opcodes[ 0x76 ] = 
-opcodes[ 0x77 ] = 
-opcodes[ 0x78 ] = 
-opcodes[ 0x79 ] = 
-opcodes[ 0x7A ] = 
-opcodes[ 0x7B ] = 
-opcodes[ 0x7C ] = 
-opcodes[ 0x7D ] = 
-opcodes[ 0x7E ] = 
-opcodes[ 0x7F ] = 
+opcodes[ 0x77 ] = ld_to_mem(H,L,A); //ld (HL),A 
+opcodes[ 0x78 ] = ld(A,B); //ld A,B
+opcodes[ 0x79 ] = ld(A,C); //ld A,C
+opcodes[ 0x7A ] = ld(A,D); //ld A,D
+opcodes[ 0x7B ] = ld(A,E); //ld A,E
+opcodes[ 0x7C ] = ld(A,H); //ld A,H
+opcodes[ 0x7D ] = ld(A,L); //ld A,L
+opcodes[ 0x7E ] = ld_from_mem(A,H,L); //ld A,(HL)
+opcodes[ 0x7F ] = ld(A,A); //ld A,A
 opcodes[ 0x80 ] = 
 opcodes[ 0x81 ] = 
 opcodes[ 0x82 ] = 
@@ -306,7 +348,7 @@ opcodes[ 0xDE ] =
 opcodes[ 0xDF ] = 
 opcodes[ 0xE0 ] = 
 opcodes[ 0xE1 ] = 
-opcodes[ 0xE2 ] = 
+opcodes[ 0xE2 ] = ld(C,A); //ld (C),A
 opcodes[ 0xE3 ] = unused
 opcodes[ 0xE4 ] = unused
 opcodes[ 0xE5 ] = 
@@ -314,7 +356,7 @@ opcodes[ 0xE6 ] =
 opcodes[ 0xE7 ] = 
 opcodes[ 0xE8 ] = unused
 opcodes[ 0xE9 ] = 
-opcodes[ 0xEA ] = 
+opcodes[ 0xEA ] = ld_to_mem_imm(A,to_imm); //ld (nn),A
 opcodes[ 0xEB ] = 
 opcodes[ 0xEC ] = unused 
 opcodes[ 0xED ] = unused
@@ -322,7 +364,7 @@ opcodes[ 0xEE ] =
 opcodes[ 0xEF ] = 
 opcodes[ 0xF0 ] = 
 opcodes[ 0xF1 ] = 
-opcodes[ 0xF2 ] = 
+opcodes[ 0xF2 ] = ld(A,C); //ld A,(C)
 opcodes[ 0xF3 ] = 
 opcodes[ 0xF4 ] = unused
 opcodes[ 0xF5 ] = 
@@ -330,7 +372,7 @@ opcodes[ 0xF6 ] =
 opcodes[ 0xF7 ] = 
 opcodes[ 0xF8 ] = 
 opcodes[ 0xF9 ] = 
-opcodes[ 0xFA ] = 
+opcodes[ 0xFA ] = ld_from_mem_imm(A); //ldd A,(nn)
 opcodes[ 0xFB ] = 
 opcodes[ 0xFC ] = unused
 opcodes[ 0xFD ] = unused
